1fb5a911891666a686e337e962ce18198469c5ca
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2024-11-02 09:46:19 +0100

    finished ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 15:09:37 by kbolon            #+#    #+#             */
+/*   Updated: 2024/11/02 09:42:41 by kbolon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+// Use & to get the address of the string
+// Reference directly refers to the original string
+int	main()
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "memory address of the string variable: " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl 
+		<< std::endl;
+
+	std::cout << "The value of the string variable: " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl 
+		<< std::endl;
+
+	return (0);
+}
