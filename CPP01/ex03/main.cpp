197653e40cca9550e19666e783bdc268b34265d8
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2024-11-03 16:41:05 +0100

    CPP01 ex04 completed

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 15:09:37 by kbolon            #+#    #+#             */
-/*   Updated: 2024/11/02 17:00:47 by kbolon           ###   ########.fr       */
+/*   Updated: 2024/11/02 17:07:18 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,6 @@
 #include "HumanA.hpp"
 #include "HumanB.hpp"
 
-
-
-
-Analyzed
-
-python
-Always show details
-
-Copy code
-# Let's first review the main.cpp file and provide comments for clarity.
-file_path_main = '/mnt/data/main.cpp'
-
-# Reading the content of main.cpp to understand and comment appropriately.
-with open(file_path_main, 'r') as file:
-    main_cpp_content = file.readlines()
-
-main_cpp_content
-Result
-['/* ************************************************************************** */\n',
- '/*                                                                            */\n',
- '/*                                                        :::      ::::::::   */\n',
- '/*   main.cpp                                           :+:      :+:    :+:   */\n',
- '/*                                                    +:+ +:+         +:+     */\n',
- '/*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */\n',
- '/*                                                +#+#+#+#+#+   +#+           */\n',
- '/*   Created: 2024/11/01 15:09:37 by kbolon            #+#    #+#             */\n',
- '/*   Updated: 2024/11/02 16:58:33 by kbolon           ###   ########.fr       */\n',
- '/*                                                                            */\n',
- '/* ************************************************************************** */\n',
- '\n',
- '#include "Weapon.hpp"\n',
- '#include "HumanA.hpp"\n',
- '#include "HumanB.hpp"\n',
- '\n',
- '\n',
- '//HumanA must have the weapon in the function call\n',
- '//HumanB must use the class function\n',
- 'int main()\n',
- '{\n',
- '\tWeapon\tclub = Weapon("crude spiked club");\n',
- '\tHumanA\tbob("Bob", club);\n',
- '\tbob.attack();\n',
- '\tclub.setType("some other type of club");\n',
- '\tbob.attack();\n',
- '\tstd::cout << std::endl << std::endl;\n',
- '\n',
- '\tWeapon\tclub1 = Weapon("crude spiked club");\n',
- '\tHumanB\tjim("Jim");\n',
- '\tjim.setWeapon(club1);\n',
- '\tjim.attack();\n',
- '\tclub1.setType("some other type of club");\n',
- '\tjim.attack();\n',
- '\treturn 0;\n',
- '}\n']
-Here's a brief explanation of the code in main.cpp to help differentiate the setups for HumanA and HumanB:
-
-cpp
-Copy code
 // HumanA requires a Weapon reference passed at the 
 //time of creation. This ensures that HumanA always 
 //has a weapon and cannot exist without one.
---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 15:09:37 by kbolon            #+#    #+#             */
+/*   Updated: 2024/11/03 16:39:29 by kbolon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Losers.hpp"
+
+//do not neet to close infile/outfile as it automatically happens when
+//they go out of scope
+//npos is used to check whether find() was successful.  If find() returns
+//something, it means s1 was found and if it returns npos then s1 was NOT found
+int main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		std::string		line;
+		std::ifstream	infile;
+		std::ofstream 	outfile;
+		std::string s1 = av[2];
+		std::string s2 = av[3];
+
+		infile.open((av[1]));
+		if (!infile)
+		{
+			std::cerr << "ERROR: Could not open infile" <<std::endl;
+			return (1);
+		}
+		outfile.open((std::string(av[1]) + ".replace"));
+		if (!outfile)
+		{
+			std::cerr << "ERROR: Could not open outfile" <<std::endl;
+			return (1);
+		}
+		while (getline(infile, line))
+		{
+			size_t	index = 0;
+			while ((index = line.find(s1, index)) != std::string::npos)
+			{
+				line = line.substr(0, index) + s2 + line.substr(index + s1.length());
+				index += s2.length(); 
+			}
+			outfile << line << std::endl;
+		}
+	}
+	else
+		std::cout << "Usage: file S1 S2" << std::endl;
+	return (0);
+}
